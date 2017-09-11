49962eb388baf899787b810077b29e434002c24d
Theron Patrick <theronbpatrick@gmail.com>
2017-09-11 09:03:20 -0400

    add road trip landing page

---
+++ roadtrip/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+
+// App wide styles
+body {
+  margin: 0;
+  font-family: 'Mukta', sans-serif;
+}
+
+body, html, #app {
+  height: 100%;
+  width: 100%;
+  position: relative;
+}
+
+h1, h2, h3, h4, h5, p, span {
+  margin: 0;
+}
+
+</style>
---
+++ roadtrip/src/components/Main.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main">
+    <div class="background-container"></div>
+    <h1>COMING SOON...</h1>
+  </div>
+</template>
+
+<script>
+
+import landscape from "../assets/landscape.jpg"
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      landscape
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.main {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+.background-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  background: url("../assets/landscape.jpg") no-repeat center center fixed;
+  background-size: cover;
+
+  filter: brightness(60%) contrast(130%);
+}
+
+h1 {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  color: #FFF;
+
+  font-size: 34px;
+  letter-spacing: 2px;
+}
+
+
+</style>
