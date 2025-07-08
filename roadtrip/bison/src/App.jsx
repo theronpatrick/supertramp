8172e8d15107fb5232577e3b236a42dd0b8a45ad
Theron Patrick <theronbpatrick@gmail.com>
2025-07-08 16:17:06 -0400

    update to d wtf

--- roadtrip/bison/src/App.jsx
+++ roadtrip/bison/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "preact/hooks";
 export function App() {
   const [count, setCount] = useState(0);
 
-  console.log("version 25.7.8c");
+  console.log("version 25.7.8d");
 
   return (
     <div className="app">
