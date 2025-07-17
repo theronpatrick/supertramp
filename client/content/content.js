9a1474c4b828d7be305ad2c92edd0fee9d89886c
Theron Patrick <theronbpatrick@gmail.com>
2025-07-17 15:54:46 -0400

    update content and resume

--- client/content/content.js
+++ client/content/content.js
@@ -1,16 +1,13 @@
-import tpr from "root/content/theron_patrick_resume.pdf"
+import tpr from "root/content/theron_patrick_resume.pdf";
 
 export default {
-  profile:
-    `<p>Hello there! My name's Theron, I'm principal engineer and tech lead at <a href="https://cargurus.com/" target="_blank">CarGurus.com</a>. I'm also an amateur game developer, adventurous cross-country traveler, and latin dance enthusiast.</p>
+  profile: `<p>Hello there! My name's Theron, I'm a senior frontend software engineer II at <a href="https://hubspot.com/" target="_blank">HubSpot</a>. I'm also an amateur game developer, adventurous cross-country traveler, and latin dance enthusiast.</p>
 
     <p>Client-side web design and development has always been my forté, but I've also dipped my toes into some back-end development (mainly in Node.js and Remix). I also love mobile development, and have experience building hybrid apps and native Android and iOS apps, with experience deploying to both respective app stores. </p>
 
     <p>Check out some of <a href="/#/projects">my work</a> for a sense of what I can do, and take a look at <a href="https://github.com/theronpatrick" target="_blank">my GitHub</a> for a sense of how I do it. And be sure to check out some of the animals, plants, and breakfast burritos I found during my <a href="http://theron.dev/roadtrip" target="_blank">three month solo cross-country adventure.</a></p>`,
-  contact:
-    `<p>To get in touch, please visit my <a href="https://www.linkedin.com/in/theronpatrick/" target="_blank">LinkedIn</a> or email at <a href="mailto:therondevelopment@gmail.com" target="_blank">therondevelopment@gmail.com</a>.</p>`,
-  projects:
-    `<p><a href="http://theron.dev/roadtrip" target="_blank">Theron's 2017 Cross-Country Extravaganza</a> - Interactive movies, pictures, and stats from my 3 month journey across America in the summer of 2017.</p>
+  contact: `<p>To get in touch, please visit my <a href="https://www.linkedin.com/in/theronpatrick/" target="_blank">LinkedIn</a> or email at <a href="mailto:therondevelopment@gmail.com" target="_blank">therondevelopment@gmail.com</a>.</p>`,
+  projects: `<p><a href="http://theron.dev/roadtrip" target="_blank">Theron's 2017 Cross-Country Extravaganza</a> - Interactive movies, pictures, and stats from my 3 month journey across America in the summer of 2017.</p>
 
     <p><a href="https://itunes.apple.com/us/app/taetro/id1195441560" target="_blank">Taetro</a> - The official mobile app of electronic music/producer <a href="https://www.youtube.com/channel/UCNRFvq0JOm5VqHIBDDQgkMA" target="_blank">Taetro</a>. Platform to share and play soundpacks created by Taetro, and to connect to the YouTube community. View on the <a href="https://play.google.com/store/apps/details?id=com.theronp.taetro&hl=en" target="_blank">Google Play Store</a> or <a href="https://itunes.apple.com/us/app/taetro/id1195441560" target="_blank">iOS App Store</a>.</p>
 
@@ -20,6 +17,5 @@ export default {
 
     <p>To view the code for some of these projects, including <a href="https://github.com/theronpatrick/supertramp" target="_blank">this website</a>, check out my <a href="https://github.com/theronpatrick" target="_blank">GitHub</a>.</p>
     `,
-  resume:
-    `<p>To review my educational background, professional project descriptions, or if you really hate trees and want to print something out, you can <a href="${tpr}" target="_blank">Download or View my PDF Resumé</a>.</p>`
-}
+  resume: `<p>To review my educational background, professional project descriptions, or if you really hate trees and want to print something out, you can <a href="${tpr}" target="_blank">Download or View my PDF Resumé</a>.</p>`,
+};
